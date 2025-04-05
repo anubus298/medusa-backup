ce10df366c5cfb6575187e9d0b7bdd1352c5a359
Ameer Rizvi <karan.raza44@gmail.com>
2025-04-05 11:52:32 +0400

    feat: backup function moved to helper

--- src/api/admin/backup/db-backup/route.ts
+++ src/api/admin/backup/db-backup/route.ts
@@ -1,77 +1,20 @@
-import path from "path";
-import os from "os";
-import {execSync} from "child_process";
-import fs from "fs";
+
 import {BACKUPS_MODULE} from "../../../../modules/backups";
-import {uploadFilesWorkflow} from "@medusajs/medusa/core-flows";
-import {createBackupZip} from "../helper";
-
-const DB_BASE = process.env.DB_BASE;
-const DB_NAME = process.env.DB_NAME;
-
-const TEMP_DIR = os.tmpdir();
-const BACKUP_FILE = path.join(TEMP_DIR, "db_backup.sql");
+import {createAndUploadBackup} from "../helper";
 
 export async function POST(req, res) {
   const service = req.scope.resolve(BACKUPS_MODULE);
 
   try {
-    if ((await service.listBackups({status: "loading"})).length > 0) {
+    const isAnyLoading =
+      (await service.listBackups({status: "loading"})).length > 0;
+    if (isAnyLoading) {
       return res
         .status(400)
         .json({error: "Backup is already in progress. Please wait."});
     }
 
-    const newBackup = await service.createBackups({status: "loading"});
-    if (!newBackup?.id) throw new Error("Failed to create a new backup entry");
-
-    const backupId = newBackup.id;
-    const DB_URL = `${DB_BASE}/${DB_NAME}`;
-    const cmd = `pg_dump -d "${DB_URL}" -f "${BACKUP_FILE}" --exclude-table-data=workflow_execution`;
-
-    execSync(cmd);
-
-    if (!fs.existsSync(BACKUP_FILE)) throw new Error("Backup file not found!");
-
-    const {zipFileName, zipFilePath} = await createBackupZip(
-      TEMP_DIR,
-      BACKUP_FILE
-    );
-    const fileBuffer = fs.readFileSync(zipFilePath);
-
-    const {result} = await uploadFilesWorkflow(req.scope).run({
-      input: {
-        files: [
-          {
-            filename: zipFileName,
-            content: fileBuffer.toString("base64"),
-            mimeType: "application/zip",
-            access: "private"
-          }
-        ]
-      }
-    });
-
-    const fileId = result?.[0]?.id || "";
-    const fileUrl = result?.[0]?.url || "";
-    if (!fileId) throw new Error("Failed to upload backup to S3");
-
-    const originalSize = fs.statSync(BACKUP_FILE).size;
-    const zipSize = fs.statSync(zipFilePath).size;
-
-    await service.updateBackups({
-      id: backupId,
-      fileId: fileId,
-      fileUrl: fileUrl,
-      status: "success",
-      metadata: {
-        size: zipSize,
-        originalSize: originalSize
-      }
-    });
-
-    fs.unlinkSync(BACKUP_FILE);
-    fs.unlinkSync(zipFilePath);
+    const {backupId, fileId, fileUrl} = await createAndUploadBackup(req.scope);
 
     return res.status(200).json({
       id: backupId,
--- src/api/admin/backup/helper.ts
+++ src/api/admin/backup/helper.ts
@@ -5,6 +5,8 @@ import * as path from "path";
 import * as os from "os";
 import {Readable} from "stream";
 import {execSync} from "child_process";
+import {uploadFilesWorkflow} from "@medusajs/medusa/core-flows";
+import {BACKUPS_MODULE} from "../../../modules/backups";
 
 const TEMP_DIR = os.tmpdir();
 
@@ -39,4 +41,66 @@ export async function createBackupZip(dir: string, file: string) {
   const zipFilePath = path.join(dir, zipFileName);
   execSync(`zip -j "${zipFilePath}" "${file}"`);
   return {zipFileName, zipFilePath};
+}
+
+export async function createAndUploadBackup(scope: any) {
+  const DB_BASE = process.env.DB_BASE;
+  const DB_NAME = process.env.DB_NAME;
+
+  const TEMP_DIR = os.tmpdir();
+  const BACKUP_FILE = path.join(TEMP_DIR, "db_backup.sql");
+
+  const service = scope.resolve(BACKUPS_MODULE);
+  const newBackup = await service.createBackups({status: "loading"});
+  if (!newBackup?.id) throw new Error("Failed to create a new backup entry");
+
+  const backupId = newBackup.id;
+  const DB_URL = `${DB_BASE}/${DB_NAME}`;
+  const cmd = `pg_dump -d "${DB_URL}" -f "${BACKUP_FILE}" --exclude-table-data=workflow_execution`;
+
+  execSync(cmd);
+
+  if (!fs.existsSync(BACKUP_FILE)) throw new Error("Backup file not found!");
+
+  const {zipFileName, zipFilePath} = await createBackupZip(
+    TEMP_DIR,
+    BACKUP_FILE
+  );
+  const fileBuffer = fs.readFileSync(zipFilePath);
+
+  const {result} = await uploadFilesWorkflow(scope).run({
+    input: {
+      files: [
+        {
+          filename: zipFileName,
+          content: fileBuffer.toString("base64"),
+          mimeType: "application/zip",
+          access: "private"
+        }
+      ]
+    }
+  });
+
+  const fileId = result?.[0]?.id || "";
+  const fileUrl = result?.[0]?.url || "";
+  if (!fileId) throw new Error("Failed to upload backup to S3");
+
+  const originalSize = fs.statSync(BACKUP_FILE).size;
+  const zipSize = fs.statSync(zipFilePath).size;
+
+  await service.updateBackups({
+    id: backupId,
+    fileId: fileId,
+    fileUrl: fileUrl,
+    status: "success",
+    metadata: {
+      size: zipSize,
+      originalSize: originalSize
+    }
+  });
+
+  fs.unlinkSync(BACKUP_FILE);
+  fs.unlinkSync(zipFilePath);
+
+  return {backupId, fileId, fileUrl};
 }
